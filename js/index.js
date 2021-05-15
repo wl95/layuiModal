8732fa2e779506321268bd7e75f71cb4d00dc072
wl <wl17610993321@163.com>
2021-05-15 19:17:34 +0800

    增加货源信息

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-function modalDom() {
-  return `
+function modalDom () {
+	return `
+	<div class="chat_modal_wrap" style="width:815px; height: 550px;">
   <div class="fl chat_left">
   <div class="chat_left_friend">
     <div class="frends select_friends">
@@ -155,65 +156,505 @@ function modalDom() {
   </div>
 </div>
 <div class="layui-icon layui-icon-close close_modal"></div>
-`
+</div>
+<div class="chat_model_right" style="height:550px; width: 780px;">
+    <div class="chat_model_table">
+      <form class="layui-form model_table_form" action="">
+        <div class="layui-form-item">
+          <div class="layui-inline">
+            <img src="./img/zu.png" alt="" style="width: 28px;">
+          </div>
+          <div class="layui-inline" id="cascader">
+            <input type="text" name="addres" required="" lay-verify="required" placeholder="请选择装货地址" autocomplete="off"
+              class="layui-input" style="width: 170px;">
+          </div>
+          <div class="layui-inline">
+            <input type="text" name="asd" required="" lay-verify="required" placeholder="请输入装货详细地址" autocomplete="off"
+              class="layui-input" style="width: 340px;">
+          </div>
+          <div class="layui-inline">
+            <input type="text" name="fff" required="" lay-verify="required" placeholder="联系人及联系方式" autocomplete="off"
+              class="layui-input" style="width: 170px;">
+          </div>
+          <div class="layui-inline">
+            <button type="button" class="layui-btn layui-btn-primary layui-btn-sm" style="height: 38px; width: 38px;">
+              <i class="layui-icon"></i>
+            </button>
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <div class="layui-inline">
+            <img src="./img/red.png" alt="" style="width: 28px;margin: 0;">
+          </div>
+          <div class="layui-inline" id="cascader1">
+            <input type="text" name="sss" required="" lay-verify="required" placeholder="请选择卸货地址" autocomplete="off"
+              class="layui-input" style="width: 170px;">
+          </div>
+          <div class="layui-inline">
+            <input type="text" name="wws" required="" lay-verify="required" placeholder="请输入卸货详细地址" autocomplete="off"
+              class="layui-input" style="width: 340px;">
+          </div>
+          <div class="layui-inline">
+            <input type="text" name="fd" required="" lay-verify="required" placeholder="联系人及联系方式" autocomplete="off"
+              class="layui-input" style="width: 170px;">
+          </div>
+          <div class="layui-inline">
+            <button type="button" class="layui-btn layui-btn-primary layui-btn-sm" style="height: 38px; width: 38px;">
+              <i class="layui-icon"></i>
+            </button>
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <div class="layui-inline">
+            <input type="text" name="ws" required="" lay-verify="required" placeholder="请输入标题" autocomplete="off"
+              class="layui-input" style="width: 200px;">
+          </div>
+          <div class="layui-inline">
+            <input type="text" name="ew" required="" lay-verify="required" placeholder="请输入标题" autocomplete="off"
+              class="layui-input" style="width: 555px;">
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <div class=" layui-inline all">
+            <div class="tag tag1 layui-inline">历史</div>
+            <div class="tag tag1 layui-inline">司机报价</div>
+            客户： <input type="text" placeholder="报价">
+            预算： <input type="text" placeholder="报价">
+          </div>
+          <div class="layui-inline" style="width: 120px;">
+            <select name="city" lay-verify="required">
+              <option value=""></option>
+              <option value="0">北京</option>
+              <option value="1">上海</option>
+              <option value="2">广州</option>
+              <option value="3">深圳</option>
+              <option value="4">杭州</option>
+            </select>
+          </div>
+          <div class="layui-inline" style="width: 230px;">
+            <input type="text" class="layui-input" id="goods-laydate" placeholder="年-月-日">
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <div class="layui-inline" style="width: 120px;">
+            <select name="city" lay-verify="required">
+              <option value=""></option>
+              <option value="0">北京</option>
+              <option value="1">上海</option>
+              <option value="2">广州</option>
+              <option value="3">深圳</option>
+              <option value="4">杭州</option>
+            </select>
+          </div>
+          <div class="tag tag2 layui-inline">标签</div>
+          <div class="layui-inline">
+            <input type="text" name="title" required="" lay-verify="required" placeholder="请输入标题" autocomplete="off"
+              class="layui-input" style="width: 226px;">
+          </div>
+          <div class="layui-inline">
+            <input type="text" name="title" required="" lay-verify="required" placeholder="请输入标题" autocomplete="off"
+              class="layui-input" style="width: 180px;">
+          </div>
+          <div class="layui-inline">
+            <button class="layui-btn layui-btn-normal" lay-submit lay-filter="formDemo" style="width:90px" id="supply_save">保存</button>
+          </div>
+          <div class="layui-inline">
+            <button type="reset"  class="layui-btn layui-btn-primary layui-border-blue" style="width:90px">清空</button>
+          </div>
+
+        </div>
+      </form>
+      <table id="chat-table" lay-filter="test"></table>
+ 		</div>
+  </div>`
 }
 
-function modalClick({ $ }) {
-  $(".tab_select span").on('click', function() {
-    var othis = $(this),
-      type = othis.data('type');
-    console.log(type)
-  })
-  $(".chat_left_friend .frends").on('click', function(e) {
-    var othis = $(this)
-    e.stopPropagation()
-    othis.siblings().removeClass('select_friends');
-    othis.addClass('select_friends')
-  })
-  $(".tab_select span").on('click', function() {
-    var othis = $(this)
-    othis.siblings().removeClass('select_tab');
-    othis.addClass('select_tab')
-  })
-  $('.chat_left_friend .friends_close').on('click', function(e) {
-    e.stopPropagation()
-    console.log(111)
-  })
+let list = [
+	{
+		value: 'zhinan',
+		label: '指南',
+		children: [
+			{
+				value: 'shejiyuanze',
+				label: '设计原则设计原则设计原则设计原则',
+				children: [
+					{
+						value: 'yizhi',
+						label: '一致',
+					},
+					{
+						value: 'fankui',
+						label: '反馈',
+					},
+					{
+						value: 'xiaolv',
+						label: '效率',
+					},
+					{
+						value: 'kekong',
+						label: '可控',
+					},
+				],
+			},
+			{
+				value: 'daohang',
+				label: '导航',
+				children: [
+					{
+						value: 'cexiangdaohang',
+						label: '侧向导航',
+					},
+					{
+						value: 'dingbudaohang',
+						label: '顶部导航',
+					},
+				],
+			},
+		],
+	},
+	{
+		value: 'zujian',
+		label: '组件',
+		children: [
+			{
+				value: 'basic',
+				label: 'Basic',
+				children: [
+					{
+						value: 'layout',
+						label: 'Layout 布局',
+					},
+					{
+						value: 'color',
+						label: 'Color 色彩',
+					},
+					{
+						value: 'typography',
+						label: 'Typography 字体',
+					},
+					{
+						value: 'icon',
+						label: 'Icon 图标',
+					},
+					{
+						value: 'button',
+						label: 'Button 按钮',
+					},
+				],
+			},
+			{
+				value: 'form',
+				label: 'Form',
+				children: [
+					{
+						value: 'radio',
+						label: 'Radio 单选框',
+					},
+					{
+						value: 'checkbox',
+						label: 'Checkbox 多选框',
+					},
+					{
+						value: 'input',
+						label: 'Input 输入框',
+					},
+					{
+						value: 'input-number',
+						label: 'InputNumber 计数器',
+					},
+					{
+						value: 'select',
+						label: 'Select 选择器',
+					},
+					{
+						value: 'cascader',
+						label: 'Cascader 级联选择器',
+					},
+					{
+						value: 'switch',
+						label: 'Switch 开关',
+					},
+					{
+						value: 'slider',
+						label: 'Slider 滑块',
+					},
+					{
+						value: 'time-picker',
+						label: 'TimePicker 时间选择器',
+					},
+					{
+						value: 'date-picker',
+						label: 'DatePicker 日期选择器',
+					},
+					{
+						value: 'datetime-picker',
+						label: 'DateTimePicker 日期时间选择器',
+					},
+					{
+						value: 'upload',
+						label: 'Upload 上传',
+					},
+					{
+						value: 'rate',
+						label: 'Rate 评分',
+					},
+					{
+						value: 'form sheet',
+						label: 'Form 表单',
+					},
+				],
+			},
+			{
+				value: 'data',
+				label: 'Data',
+				children: [
+					{
+						value: 'table',
+						label: 'Table 表格',
+					},
+					{
+						value: 'tag',
+						label: 'Tag 标签',
+					},
+					{
+						value: 'progress',
+						label: 'Progress 进度条',
+					},
+					{
+						value: 'tree',
+						label: 'Tree 树形控件',
+					},
+					{
+						value: 'pagination',
+						label: 'Pagination 分页',
+					},
+					{
+						value: 'badge',
+						label: 'Badge 标记',
+					},
+				],
+			},
+			{
+				value: 'notice',
+				label: 'Notice',
+				children: [
+					{
+						value: 'alert',
+						label: 'Alert 警告',
+					},
+					{
+						value: 'loading',
+						label: 'Loading 加载',
+					},
+					{
+						value: 'message',
+						label: 'Message 消息提示',
+					},
+					{
+						value: 'message-box',
+						label: 'MessageBox 弹框',
+					},
+					{
+						value: 'notification',
+						label: 'Notification 通知',
+					},
+				],
+			},
+			{
+				value: 'navigation',
+				label: 'Navigation',
+				children: [
+					{
+						value: 'menu',
+						label: 'NavMenu 导航菜单',
+					},
+					{
+						value: 'tabs',
+						label: 'Tabs 标签页',
+					},
+					{
+						value: 'breadcrumb',
+						label: 'Breadcrumb 面包屑',
+					},
+					{
+						value: 'dropdown',
+						label: 'Dropdown 下拉菜单',
+					},
+					{
+						value: 'steps',
+						label: 'Steps 步骤条',
+					},
+				],
+			},
+			{
+				value: 'others',
+				label: 'Others',
+				children: [
+					{
+						value: 'dialog',
+						label: 'Dialog 对话框',
+					},
+					{
+						value: 'tooltip',
+						label: 'Tooltip 文字提示',
+					},
+					{
+						value: 'popover',
+						label: 'Popover 弹出框',
+					},
+					{
+						value: 'card',
+						label: 'Card 卡片',
+					},
+					{
+						value: 'carousel',
+						label: 'Carousel 走马灯',
+					},
+					{
+						value: 'collapse',
+						label: 'Collapse 折叠面板',
+					},
+				],
+			},
+		],
+	},
+	{
+		value: 'ziyuan',
+		label: '资源',
+		children: [
+			{
+				value: 'axure',
+				label: 'Axure Components',
+			},
+			{
+				value: 'sketch',
+				label: 'Sketch Templates',
+			},
+			{
+				value: 'jiaohu',
+				label: '组件交互文档',
+			},
+		],
+	},
+	{ value: 1, label: 'xxx' },
+];
+
+function modalClick ({ $ }) {
+	$(".tab_select span").on('click', function () {
+		var othis = $(this),
+			type = othis.data('type');
+		console.log(type)
+	})
+	$(".chat_left_friend .frends").on('click', function (e) {
+		var othis = $(this)
+		e.stopPropagation()
+		othis.siblings().removeClass('select_friends');
+		othis.addClass('select_friends')
+	})
+	$(".tab_select span").on('click', function () {
+		var othis = $(this)
+		othis.siblings().removeClass('select_tab');
+		othis.addClass('select_tab')
+	})
+	$('.chat_left_friend .friends_close').on('click', function (e) {
+		e.stopPropagation()
+		console.log(111)
+	})
 }
 
-layui.use('layer', function() {
-  let chat_modal = null
-  var $ = layui.jquery
-  var layer = layui.layer
-  modalClick({ $ })
-    // chat_modal = layer.open({
-    //   title: false,
-    //   closeBtn: false,
-    //   btn: false,
-    //   skin: 'chat_modal_wrap',
-    //   moveType: 1,
-    //   area: ['815px', '550px'],
-    //   content: modalDom()
-    // });
-    // close_modal = $(".close_modal")
-  $(".open_chat").on('click', function() {
-    chat_modal = layer.open({
-      title: false,
-      closeBtn: false,
-      btn: false,
-      skin: 'chat_modal_wrap',
-      moveType: 1,
-      area: ['815px', '550px'],
-      content: modalDom()
-    });
-    modalClick({ $ })
-    $(".close_modal").on('click', function() {
-      layer.closeAll()
-    })
-    $(".chat_left_friend .frends").on('click', function() {
-      var othis = $(this)
-      othis.siblings().removeClass('select_friends');
-      othis.addClass('select_friends')
-    })
-  })
+layui.use(['form', 'layedit', 'laydate', 'layer', 'table'], function () {
+	let chat_modal = null
+	var $ = layui.jquery
+	var table = layui.table
+	var layer = layui.layer
+	var form = layui.form
+	modalClick({ $ })
+	// chat_modal = layer.open({
+	//   title: false,
+	//   closeBtn: false,
+	//   btn: false,
+	//   skin: 'chat_modal_wrap',
+	//   moveType: 1,
+	//   area: ['815px', '550px'],
+	//   content: modalDom()
+	// });
+	// close_modal = $(".close_modal")
+	$(".open_chat").on('click', function () {
+		chat_modal = layer.open({
+			title: false,
+			closeBtn: false,
+			btn: false,
+			skin: 'chat_modal_wrap',
+			moveType: 1,
+			area: ['1600px', '550px'],
+			success: function(layero, index){
+				// 重新渲染弹层中的下拉选择框select
+				form.render('select');
+				modalClick({ $ })
+				$(".close_modal").on('click', function () {
+					layer.closeAll()
+				})
+				$(".chat_left_friend .frends").on('click', function () {
+					var othis = $(this)
+					othis.siblings().removeClass('select_friends');
+					othis.addClass('select_friends')
+				})
+				table.render({
+					elem: '#chat-table'
+					, height: 312
+					// , url: '/demo/table/user/' //数据接口
+					// , page: true //开启分页
+					, cols: [[ //表头
+						{ field: 'action', title: '出发地-目的地', width: 150 }
+						, { field: 'detail', title: '货车及用车详情', width: 150 }
+						, { field: 'tel', title: '手机号码', width: 100 }
+						, { field: 'price', title: '客户出价', width: 100 }
+						, { field: 'time', title: '处理日期及状态', width: 130 }
+						, { field: 'person', title: '操作人', width: 140 }
+					]]
+				});
+				form.on('submit(formDemo)', function (data) {
+					layer.msg(JSON.stringify(data.field));
+					return false;
+				});
+				const cas = new Cascader({
+					container: '#cascader',
+					data: arr,
+					selectFn: (val) => {
+						console.log('你选择了', val);
+					},
+				});
+				const xcas = new Cascader({
+					container: '#cascader1',
+					data: arr,
+					selectFn: (val) => {
+						console.log('你选择了', val);
+					},
+				});
+				$('#cascader input').on('focus', function () {
+					console.log('你选择了', 'cascader');
+					xcas.popClose()
+				})
+				$('#cascader1 input').on('focus', function () {
+					console.log('你选择了', 'cascader1');
+					cas.popClose()
+				})
+			},
+			content: modalDom()
+		});
+	})
 });
+
+layui.use('laydate', function () {
+	var laydate = layui.laydate
+	laydate.render({
+		elem: '#goods-laydate',
+		btns: ['clear'],
+		change: function (value, date, endDate) {
+			console.log(value); //得到日期生成的值，如：2017-08-18
+			console.log(date); //得到日期时间对象：{year: 2017, month: 8, date: 18, hours: 0, minutes: 0, seconds: 0}
+			console.log(endDate); //得结束的日期时间对象，开启范围选择（range: true）才会返回。对象成员同上。
+		}
+	});
+});
+
